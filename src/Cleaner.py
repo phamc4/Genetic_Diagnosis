14d6c8a2b8923de6307f32ddb982c7837b636f92
Tu Pham <phamckm@gmail.com>
2020-05-13 10:02:12 -0700

    Update function

--- src/Cleaner.py
+++ src/Cleaner.py
@@ -1,18 +1,24 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 def data_cleaner(df):
+    cleaned_df = df.copy()
     
     #Encode Categorical Features
-    df['type'] = (df['type'] == 'normal').astype(int)
-    
+    if len(cleaned_df['type'].unique()) == 2:
+        cleaned_df['type'] = (cleaned_df['type'] == 'normal').astype(int)
+    else:
+        lb = LabelEncoder()
+        cleaned_df['type'] = lb.fit_transform(cleaned_df['type'])
+        
     #Drop Columns
-    df.drop(columns='samples', inplace=True)
+    cleaned_df.drop(columns='samples', inplace=True)
     
     #Rename target header
-    df.rename({'type': 'target'}, inplace=True)
-    
-    return df
+    cleaned_df.rename({'type': 'target'}, axis=1, inplace=True)
+        
+    return cleaned_df
 
 def labels(df):
     #Get the target labels in the dataframe
